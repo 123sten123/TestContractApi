da89b1a35814c0c81cc403783f2ab40f801f69ce
Pozolotin Stepan <Pozolotin.stepan@gmail.com>
2021-01-30 17:06:21 +0400

    add flyway

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,11 @@
+create table contract(
+  id int NOT NULL,
+  date_contract date,
+  number int,
+  date_chek date
+);
+
+insert into contract(id,date_chek, date_contract, number) values (1,'20-01-2021', '15-01-2021', 1);
+insert into contract(id,date_chek, date_contract, number) values (2,'02-05-2018', '17-07-2015', 2);
+insert into contract(id,date_chek, date_contract, number) values (3,'20-12-2020', '15-01-2020', 3);
+insert into contract(id,date_chek, date_contract, number) values (4, '20-01-2021', '15-01-2021', 4);
